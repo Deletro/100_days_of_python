c37c2ba98d239db850f828f5c962aeebdce72e4a
Akos Nagy Gellai <nagygellai93@gmail.com>
2023-05-03 16:18:57 +0200

    added day 1 exercises

---
+++ 001-band-name-generator/main.py
@@ -0,0 +1,12 @@
+# 1. Create a greeting for your program.
+print("Hi Ákos")
+
+# 2. Ask the user for the city that they grew up in.
+city = input("Where do u live?\n")
+
+# 3. Ask the user for the name of a pet.
+pet_name = input("What is your favourite pet name?\n")
+
+# 4. Combine the name of their city and pet and show them their band name.
+band_name = city + pet_name
+print(f"Your band name: {city} {pet_name}")
