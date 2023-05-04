e0803365fc80c6404cfd4d2f4751889b17acd09e
Akos Nagy Gellai <nagygellai93@gmail.com>
2023-05-04 19:42:21 +0200

    making it a bit more interactive

--- 001-band-name-generator/main.py
+++ 001-band-name-generator/main.py
@@ -1,12 +1,12 @@
 # 1. Create a greeting for your program.
-print("Hi Ákos")
+name = input("Hey rockstar! How can i call you?\n")
+print(f"Hi {name}, let's see some questions, and i'll help you to choose the right name to your band")
 
 # 2. Ask the user for the city that they grew up in.
-city = input("Where do u live?\n")
+city = input(f"{name}, tell me please where do u live?\n")
 
 # 3. Ask the user for the name of a pet.
-pet_name = input("What is your favourite pet name?\n")
+pet_name = input(f"And the last question to you {name} what is your favourite pet name?\n")
 
 # 4. Combine the name of their city and pet and show them their band name.
-band_name = city + pet_name
-print(f"Your band name: {city} {pet_name}")
+print(f"So {name}, i strongly recommend the '{city} {pet_name}' to your band name!")
