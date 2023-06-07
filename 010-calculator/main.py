1d2da4d6fc55a1c49d99e4fabe1dc44b6abacf5e
Akos Nagy Gellai <nagygellai93@gmail.com>
2023-06-07 20:33:12 +0200

    indentation fix and blackjack start

--- 010-calculator/main.py
+++ 010-calculator/main.py
@@ -1,34 +1,27 @@
 # calculator functions
+
+
 # Add
-
-
 def add(n1, n2):
-    print(f"Result: {n1+n2}")
+    return n1 + n2
 
 
 # Subtract
-
-
 def subtract(n1, n2):
-    print(f"Result: {n1-n2}")
+    return n1 - n2
 
 
 # Multiply
-
-
 def multiply(n1, n2):
-    print(f"Result: {n1*n2}")
+    return n1 * n2
 
 
 # Divide
-
-
 def divide(n1, n2):
-    print(f"Result: {n1/n2}")
+    return n1 / n2
 
 
 # dict what contains the functions and the operators
-
 operations = {
     "+": add,
     "-": subtract,
@@ -38,13 +31,12 @@ operations = {
 
 
 keep_going = "yes"
-
-
 while keep_going == "yes":
     num1 = int(input("First number:\n"))
     function = operations[input("Choose the operation:\n'+', '-', '*', '/':\n")]
     num2 = int(input("Second number:\n"))
-    function(n1=num1, n2=num2)
+    result = function(n1=num1, n2=num2)
+    print(f"Result: {result}")
     loop_breaker = input("Do you want to continue?:\n 'yes', 'no':\n")
     if loop_breaker == "yes":
         continue
