02106496fc4acca7f544c4901ff73d7a43a80cd5
Akos Nagy Gellai <nagygellai93@gmail.com>
2023-06-16 19:23:50 +0200

    number guessing game modified

--- 012-number-guessing-game/main.py
+++ 012-number-guessing-game/main.py
@@ -1,5 +1,9 @@
 import random
 
+
+EASY_LIFE_NUM = 10
+HARD_LIFE_NUM = 5
+
 print("Welcome to the Number Guessing Game!")
 
 chosen_number = random.randint(1, 100)
@@ -13,45 +17,19 @@ def evaluater(num1, num2=chosen_number):  # kiértékelő function
         print("Too low")
 
 
-def life_check(life):
-    if life == 0:
-        print("Game Over")
-    else:
+def play(life):
+    while life != 0:
         print(f"You have {life} life")
-
-
-def play_easy():
-    life = 10
-
-    while life != 0:
-        life_check(life)
         guess = int(input("Guess a number: "))
 
         if guess == chosen_number:
             print("You got it")
             break
-        elif guess != chosen_number and life == 1:
+        elif life == 1:
             print("You run out of guesses, you lose.")
+            print(f"(The number was: {chosen_number})")
             break
-        elif guess != chosen_number:
-            evaluater(guess, chosen_number)
-            life -= 1
-
-
-def play_hard():
-    life = 5
-
-    while life != 0:
-        life_check(life)
-        guess = int(input("Guess a number: "))
-
-        if guess == chosen_number:
-            print("You got it")
-            break
-        elif guess != chosen_number and life == 1:
-            print("You run out of guesses, you lose.")
-            break
-        elif guess != chosen_number:
+        else:
             evaluater(guess, chosen_number)
             life -= 1
 
@@ -59,8 +37,8 @@ def play_hard():
 difficulty = input("Choose a difficulty: 'easy' or 'hard': ")
 
 if difficulty == "easy":
-    play_easy()
+    play(EASY_LIFE_NUM)
 elif difficulty == "hard":
-    play_hard()
+    play(HARD_LIFE_NUM)
 else:
     print("I dont found that kind of difficulty")
