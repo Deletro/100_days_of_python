77818eb0171a13e1a74d4864c68ac5b354901e81
Akos Nagy Gellai <nagygellai93@gmail.com>
2023-06-18 14:53:48 +0200

    higher lower game

---
+++ 013-higher-lower-game/main.py
@@ -0,0 +1,53 @@
+from art import *
+from game_data import data
+import random
+import os
+
+
+def clear():  # Cross-platform clear screen
+    os.system("cls" if os.name == "nt" else "clear")
+
+
+def checker(compare_a_followers, compare_b_followers):
+    if compare_a_followers > compare_b_followers:
+        return "A"
+    else:
+        return "B"
+
+
+def play():
+    compare_b = random.choice(data)
+    points = 0
+
+    while True:
+        print(logo)
+        compare_a = compare_b
+        compare_b = random.choice(data)
+        while compare_a == compare_b:
+            compare_b = random.choice(data)
+
+        compare_a_followers = compare_a["follower_count"]
+        compare_b_followers = compare_b["follower_count"]
+
+        print(
+            f"Compare A: {compare_a['name']}, a {compare_a['description']}, from {compare_a['country']}"
+        )
+        print(f"hidden score: {compare_a_followers}")
+        print(vs)
+        print(
+            f"Compare B: {compare_b['name']}, a {compare_b['description']}, from {compare_b['country']}"
+        )
+        print(f"hidden score: {compare_b_followers}")
+
+        guess = input("Who has more followers? Type 'A' or 'B': ").upper()
+        winner = checker(compare_a_followers, compare_b_followers)
+
+        if guess == winner:
+            points += 1
+            print(f"You are right! Current score {points}.")
+        else:
+            print(f"Sorry, that's wrong, Final score: {points}.")
+            break
+
+
+play()
