31a285f9f7a398310cd2c1f3f0cf7bf5ac89e254
Akos Nagy Gellai <nagygellai93@gmail.com>
2023-06-18 14:59:48 +0200

    clear removed

--- 013-higher-lower-game/main.py
+++ 013-higher-lower-game/main.py
@@ -1,11 +1,6 @@
-from art import *
+from art import logo, vs
 from game_data import data
 import random
-import os
-
-
-def clear():  # Cross-platform clear screen
-    os.system("cls" if os.name == "nt" else "clear")
 
 
 def checker(compare_a_followers, compare_b_followers):
